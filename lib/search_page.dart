fc4ab9452420fee1418b59f1602dee59980242e9
Pratham Shah <prathamshah0506@gmail.com>
2026-08-16 21:03:39 +0530

    v2.0.1

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -419,7 +419,7 @@ class _SearchPageState extends ConsumerState<SearchPage> {
                           height: 1.1)),
                   const SizedBox(height: 16),
                   const Text(
-                      'A place to explore the world\'s largest free media archive.\nEngineered specifically for scientists and researchers to navigate, filter, and extract media intuitively and effortlessly.',
+                      'A place to explore the world\'s largest free media archive.\nEngineered for students and researchers to navigate, filter, and extract media intuitively and effortlessly.',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                           color: Color(0xFFCCCCCC),
